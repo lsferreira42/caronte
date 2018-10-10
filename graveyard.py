bf0a889ea3d16d1034be5f5b50403b9e984340da
Leandro Ferreira <leandrodsferreira@gmail.com>
2018-10-10 18:37:24 -0300

    a lot of things

--- graveyard.py
+++ graveyard.py
@@ -13,6 +13,7 @@ from Queue import Queue
 from threading import Thread
 from numbers import Number
 from collections import Set, Mapping, deque
+import ssl
 
 # We need to measure our python object sizes
 try: # Python 2
@@ -29,8 +30,12 @@ api = Flask(__name__)
 #Global objects
 global log_queue
 global log_object
+global connection_pool
+global client_logs_pool
 log_queue = deque()
 log_object = {}
+connection_pool = {}
+client_logs_pool = {}
 
 # Logging config
 logging.basicConfig(level=logging.INFO, format='%(name)s:[%(levelname)s]: %(message)s')
@@ -123,9 +128,8 @@ class ClientHandler(asyncore.dispatcher):
                 try:
                     log_json = json.loads(i, strict=False)
                     log_queue.append(log_json)
-                    logging.info("rolou")
                 except Exception as err:
-                    print err
+                    logging.debug(err)
         #if data[-1:].decode("utf-8") == '\n':
         #    self.logger.debug('handle_read() -> (%d) "%s"', len(data), data)
         
@@ -134,20 +138,65 @@ class ClientHandler(asyncore.dispatcher):
         self.logger.debug('handle_close()')
         self.close()
 
+def set_keepalive_linux(sock, after_idle_sec=300, interval_sec=120, max_fails=10):
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+    return
+
 def getsize(in_object):
     return sys.getsizeof(in_object)
 
 def bytes_to_mbytes(in_bytes):
     return ('{:,.0f}'.format(in_bytes/float(1<<20)))
 
+def create_socket(host, port, protocol):
+    logging.info("New connection to: {0} {1} in {2} mode.".format(host, port, protocol))
+    if protocol == "udp":
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #s.setblocking(0)
+    set_keepalive_linux(s)
+    s.connect((host, port))
+    if protocol == "tls":
+        return ssl.wrap_socket(s, None, None)
+    return s
+
+
+def send_log(log_object):
+    global connection_pool
+    sslopt = False
+    host_port_protocol = log_object["metadata"].replace(" ", ":")
+    host, port, protocol = host_port_protocol.split(":")
+    try:
+        if not connection_pool.get(host_port_protocol):
+            connection_pool[host_port_protocol] = create_socket(host, int(port), protocol)
+    except Exception as err:
+        logging.info(err)
+
+    try:
+        if protocol == "udp":
+            connection_pool[host_port_protocol].sendto(log_object["log"] + '\r\n', (host, int(port)))
+            return
+        connection_pool[host_port_protocol].sendall(log_object["log"] + '\r\n')
+    except Exception as err:
+        logging.info(err)
+    return
+
 
 def run_send_loop():
     """ Loop to send the logs to the clients """
     global log_queue
     global log_object
-    for i in log_queue:
-        logging.info(i)
-    sleep(1)
+    while True:
+        try:
+            if len(log_queue) > 0:
+                send_log(log_queue.pop())
+        except Exception as err:
+            logging.info(err)
+            
     pass
 
 def run_loop(host, port):
@@ -169,14 +218,24 @@ def api_start():
 
 def api_queuelist():
     """ Api Method that return the number of remaining itens in queue """
-    return str(len(link_queue)) + '\n'
+    return str(len(log_queue)) + '\n'
+
+@api.route('/api/v1/<api_module>', methods=['GET', 'POST'])
+def main_api(api_module):
+    """ Main API function, it'll try to execute the function that match
+    with the api_module flask rest method"""
+    try:
+        return getattr(sys.modules[__name__], "api_{0}".format(api_module))()
+    except Exception as error:
+        return 'Api module not found or not implemented!\n{0}\n'.format(error)
 
 
 def main():
-    pool = ThreadPool(12)
+    pool = ThreadPool(32)
     pool.add_task(run_loop, "0.0.0.0", 5514)
     pool.add_task(api.run, host='0.0.0.0', port=5000)
-    for i in range(1, 9):
+    for i in range(1, 30):
+        logging.info("Sending worker {0} to the pool".format(i))
         pool.add_task(run_send_loop)
     while core_config["terminate"] is False:
         try:
